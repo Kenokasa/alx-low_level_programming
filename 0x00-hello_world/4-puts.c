105b9a73a562540dbfeff3c3fa96071ad682ddff
Kenokasa <kenokasa2035@gmail.com>
2022-07-01 00:08:54 -0700

    using pus

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,8 @@
 #!/bin/bash
-/*this is liberary*/
 #include <stdio.h>
-/*main function*/
 int main(void)
 {
-/*printing without printf but using puts*/
 puts("\"Programming is like building a multilingual puzzle");
-/*returning 0 */
 return (0);
 
 }
