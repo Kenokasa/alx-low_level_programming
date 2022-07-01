1c8e5392f06ea925a4d40dda99d077bb3a81397e
Kenokasa <kenokasa2035@gmail.com>
2022-07-01 00:02:43 -0700

    printing using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+/*this is liberary*/
+#include <stdio.h>
+/*main function*/
+int main(void)
+{
+/*printing without printf but using puts*/
+puts("\"Programming is like building a multilingual puzzle");
+/*returning 0 */
+return (0);
+
+}
