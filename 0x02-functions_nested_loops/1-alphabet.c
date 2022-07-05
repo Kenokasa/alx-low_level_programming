884b1938f85becbeb6aa1798a0268a09309a91d3
Kenokasa <kenokasa2035@gmail.com>
2022-07-05 11:54:33 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,15 @@
 /**
  * main - check alphabet
  *
+ * Description: Prints the alphabet with _putchar
+ *
  * Return: Always (0)
  */
 
 void print_alphabet(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
 	{
 		_putchar(c);
