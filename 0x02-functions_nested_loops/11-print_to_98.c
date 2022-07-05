8ba8c8cfd15031b846b19487b1abae6e14cc8584
Kenokasa <kenokasa2035@gmail.com>
2022-07-05 13:14:41 -0700

    hot fixs

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,5 @@
-#include "holberton.h"
-
+#include "main.h"
 #include <stdio.h>
-
-
 
 /**
  * print_to_98 - prints from any integer to 98
@@ -12,28 +9,15 @@
  * Return: void
  */
 
-
-
 void print_to_98(int n)
-
 {
-
 	while (n != 98)
-
 	{
-
 		printf("%i, ", n);
-
 		if (n > 98)
-
 			n--;
-
 		else
-
 			n++;
-
 	}
-
 	printf("98\n");
-
 }
