4ff83775b028f1f8e461dad69c5ba5aaef9ff0c1
Kenokasa <kenokasa2035@gmail.com>
2022-07-05 11:08:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/**
+ * main - prints _putchar
+ *
+ * Return: Always (0) 
+ */
+
+int main(void)
+{
+	int i = 0;
+	char c;
+	char k[] = "_putchar\n";
+
+	for (i ; i=8 ; i++)
+	{
+		c = k[i];
+		_putchar(c);
+	}
+
+	return (0);
+}
