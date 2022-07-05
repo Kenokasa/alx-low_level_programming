73da230e386ef3d0c974994ad6449791c271a5f4
Kenokasa <kenokasa2035@gmail.com>
2022-07-05 11:45:46 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check alphabet
+ *
+ * Return: Always (0)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	return (0);
+}
